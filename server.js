05e432f9ef2871ac5bb3d2f150744765e83ff0c4
tarek421995 <tarek421995@gmail.com>
2020-11-14 00:15:52 +0200

    make it better

---
+++ server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import mongoose from "mongoose";
+import Data from "./data.js";
+import Videos from "./dbModel.js";
+
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+//ad
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"),
+    res.setHeader("Access-Control-Allow-Headers", "*"),
+    next();
+});
+
+//DB config
+const connection_url =
+  "mongodb+srv://admin:QhPg7YRuMlupRSa8@cluster0.kopa9.mongodb.net/tiktokDb?retryWrites=true&w=majority";
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+//api endpoints
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
+
+app.get("/v2/posts", (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/v2/posts", (req, res) => {
+  const dbVideos = req.body;
+
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+//listen
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
